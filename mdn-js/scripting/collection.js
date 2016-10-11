422a74dd09c90c2c5d7c431d30d9f60dbf208bed
Muthu kumaran T <monstershr@gmail.com>
2016-10-11 20:13:40 +0530

    Added number file

---
+++ mdn-js/scripting/collection.js
@@ -0,0 +1,15 @@
+/* Numbers */
+console.log("/* Numbers */");
+console.log(Number.MAX_VALUE);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(-2*-4);
+
+/* Date */
+console.log("/* Date Object */");
+var date=new Date();
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getMonth());
+console.log(date.getTime());
+console.log(date.getUTCDate());
+ 
